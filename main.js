3e53c72feb7e0a4417619f6f1f50bfbf7f224ce9
xSwezan <101433236+xSwezan@users.noreply.github.com>
2022-09-18 19:44:26 +0200

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+	const Button = document.getElementById("MyProfile");
+
+	Button.addEventListener("click", () => {
+		window.open("https://www.roblox.com/users/116387673/profile");
+	});
+})
